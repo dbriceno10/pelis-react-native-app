6bfbdf7488274d6df8e9d75f6d81e745112d6d8d
Daniel Briceno <daniel.b100726@gmail.com>
2022-10-17 17:40:12 -0400

    avances buscador de peliculas

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, ScrollView, View } from "react-native";
+import { StyleSheet, ScrollView, View, TextInput, Button } from "react-native";
 import axios, { AxiosResponse } from "axios";
-
 import EditScreenInfo from "../components/EditScreenInfo";
 // import { Text, View } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
@@ -10,30 +9,107 @@ import Card from "../components/Card";
 
 const URL_BASE = "http://www.omdbapi.com/?i=tt3896198&apikey=1bc6c554";
 
+let timeId: any = null;
+
 export default function TabOneScreen({
   navigation,
 }: RootTabScreenProps<"TabOne">) {
-  const [data, setData] = useState<Movie[]>([]);
-  const getData = async () => {
-    const pelis: AxiosResponse<ResponseMovies> = await axios.get(
-      `${URL_BASE}&s=avengers`
-    );
-    setData(pelis.data.Search);
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [search, setSearch] = useState<string>("");
+  const [page, setPage] = useState<number>(1);
+  const getData = async (search?: string, page?: number) => {
+    try {
+      let searchText = search;
+      if (!searchText) searchText = "avengers";
+      let query = `&s=${searchText}`;
+      if (page) query = `${query}&page=${page}`;
+      const newMovies: AxiosResponse<ResponseMovies> = await axios.get(
+        `${URL_BASE}${query}`
+      );
+      console.log(newMovies);
+      if (newMovies.data.Search) {
+        setMovies(newMovies.data.Search);
+      } else {
+        throw new Error("No se encontraron resultados");
+      }
+      return newMovies;
+    } catch (error) {
+      alert(error);
+      console.error(error);
+    }
   };
   useEffect(() => {
     getData();
   }, []);
-  console.log(data);
-  console.log(JSON.stringify(data));
+
+  const handleSearch = (e: any) => {
+    clearTimeout(timeId);
+    setSearch(e.target.value);
+    timeId = setTimeout(async() => {
+      try {
+        const newMovies = await getData(e.target.value);
+        if(!newMovies?.data.Search || newMovies === undefined) throw new Error()
+        setPage(1)
+      } catch (error) {
+        
+      }
+    }, 3000);
+  };
+
+  const nextPage = async () => {
+    try {
+      const newPage = page + 1;
+      const newMovies = await getData(search, newPage);
+      if (!newMovies?.data.Search || newMovies === undefined)
+        throw new Error("No hay más peliculas");
+      setPage(newPage);
+    } catch (error) {
+      alert(error);
+      console.error(error);
+      setPage(page - 1);
+    }
+  };
+
+  const prevPage = async () => {
+    try {
+      const newPage = page - 1;
+      if (newPage < 1) throw new Error("Está en la primera página");
+      await getData(search, newPage);
+      setPage(newPage);
+    } catch (error) {
+      alert(error);
+      console.error(error);
+      setPage(1);
+    }
+  };
+
   return (
     <View style={styles.container}>
+      <TextInput
+        value={search}
+        onChange={handleSearch}
+        style={{ backgroundColor: "#fff", marginTop: "10px", width: '60%',borderRadius: 4 }}
+      />
+
       <ScrollView>
-        {data?.map((movie) => (
+        {movies?.map((movie) => (
           <View key={movie.imdbID}>
             <Card movie={movie} />
           </View>
         ))}
       </ScrollView>
+      <View style={{ display: "flex", flexDirection: "row", marginBottom: "10px" }}>
+        <Button title="Anterior" onPress={prevPage} />
+        <Button title="Siguiente" onPress={nextPage} />
+      </View>
+      <Button
+        title="Resetear"
+        onPress={() => {
+          getData();
+          setSearch("");
+          setPage(1);
+        }}
+      />
     </View>
   );
 }
